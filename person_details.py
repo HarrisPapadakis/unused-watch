35b73b80e3351d75e32e6cded118c0f480d23e90
Harris_Papadakis <162270644+HarrisPapadakis@users.noreply.github.com>
2025-01-01 23:25:19 +0200

    Create person_details.py

---
+++ person_details.py
@@ -0,0 +1,11 @@
+person = {
+    "First name": "Naruto",
+    "Last name": "Uzumaki",
+    "Age": "18",
+    "Town": "Konoha",
+    "Master move": "Rasengan"
+}
+
+# Loop through key-value pairs in the dictionary
+for key, value in person.items():  # Fixed the typo from "item" to "items"
+    print(f"{key}: {value}")
